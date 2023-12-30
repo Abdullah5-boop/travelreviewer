cbea64adc7d70b61db0c5e37ea16f859127a43bf
Abdullah Al Mahmood <abdullahalmahmood500@gmail.com>
2023-12-30 10:16:10 +0600

    update database

--- index.js
+++ index.js
@@ -25,10 +25,110 @@ app.get('/person', (req, res) => {
         res.send(result)
     })
 })
+app.get('/hotel/:id', (req, res) => {
+    const data = req?.params;
+    console.log(data.id)
+    const hotels = `
+    
+    SELECT 
+    r.personidaemail,
+    r.hrid,
+    r.reviewtext,
+    p.personidaemail,
+    p.personname,
+    p.personlocation,
+    h.hrid,
+    h.country,
+    h.price,
+    h.hotelname
+    FROM review r JOIN persontbl p ON r.personidaemail = p.personidaemail 
+    JOIN hotelrestaurant h ON h.hrid = r.hrid 
+    WHERE r.hrid= '${data.id}'
+    
+    
+    
+    `
+
+
+    pool.query(hotels, (err, result) => {
+        if (err) {
+            console.log("we got error ")
+        }
+        res.send(result)
+    })
+
+})
+
+app.get('/identity/:id',(req,res)=>{
+    const value= req.params.id;
+    const querry=`SELECT * FROM hotelrestaurant WHERE hrid='${value}'`
+    pool.query(querry,(err,result)=>{
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    })
+})
+
+
+app.get('/login/:email', (req, res) => {
+    const data = req?.params.email;
+
+    let queries = ` SELECT COUNT(*) as count FROM persontbl WHERE personidaemail='${data}'`
+    pool.query(queries, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(result[0]);
+
+        res.send(result[0])
+    })
+})
+app.get('/signup/:email', (req, res) => {
+    const data = req?.params.email;
+
+    let queries = ` SELECT COUNT(*) as count FROM persontbl WHERE personidaemail='${data}'`
+    pool.query(queries, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(result[0]);
+
+        res.send(result[0])
+    })
+})
+app.post('/signuppost', (req, res) => {
+    const data = req?.body;
+    console.log(data);
+    const querry = `INSERT INTO persontbl(personidaemail, personname) VALUES ('${data?.email}','${data?.name}')`
+    pool.query(querry, (err, result) => {
+        if (err) { console.log(err) }
+        res.send(result)
+    })
+
+})
+
+app.post('/reviewpost', (req, res) => {
+    const data = req.body
+    let querry = `
+
+    INSERT INTO review(personidaemail, hrid, qs1, reviewtext) 
+    VALUES ('${data.email}','${data.hotleid}','1','${data.cmnt}')
+
+    `
+    pool.query(querry, (err, result) => {
+        if (result) { console.log(result) }
+        res.send(result)
+    })
+
+    console.log(data)
+})
+
 app.get('/country/:name', (req, res) => {
     let data = req.params?.name;
     let query = `SELECT * FROM hotelrestaurant WHERE country='${data}'`
     pool.query(query, (err, result) => {
+
         if (err) {
             console.log("we got error ")
         }
